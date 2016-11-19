83790b58832f7decd4364e8255cf23d82b7ac634
lshapz <laurashapiro01@gmail.com>
2016-11-18 19:38:11 -0500

    generate models and controllers

---
+++ app/controllers/task_controller.rb
@@ -0,0 +1,13 @@
+class TaskController < ApplicationController
+  def index
+  end
+
+  def view
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  get 'task/index'
+
+  get 'task/view'
+
+  get 'task/new'
+
+  get 'task/edit'
+
+  get 'issues/show'
+
+  get 'issues/index'
+
+  get 'issues/view'
+
+  get 'sessions/new'
+
+  get 'sessions/show'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
