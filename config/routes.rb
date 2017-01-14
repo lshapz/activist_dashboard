42bcf8bde2e29f3fbde298c2811c34525b01ffaf
lshapz <laurashapiro01@gmail.com>
2017-01-14 13:24:45 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks
   resources :users
-  resources :sessions, except: [:create, :new]
-  post '/login', to: "sessions#create"  
-  get 'login', to: "sessions#new"
+  resources :sessions
   resources :issues 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
